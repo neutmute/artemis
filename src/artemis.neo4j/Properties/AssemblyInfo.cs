52bd43a190eaa19baa61a277ce95c59539a44911
neutmute <neutmute@users.noreply.github.com>
2016-06-07 15:02:49 +1000

    seed project

---
+++ src/artemis.neo4j/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("artemis.neo4j")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nine Entertainment Co Pty Ltd")]
+[assembly: AssemblyProduct("artemis.neo4j")]
+[assembly: AssemblyCopyright("Copyright © Nine Entertainment Co Pty Ltd 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c17e57a-ad8f-41b8-aa85-5ae83cc303b3")]
